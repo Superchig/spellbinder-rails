30046b60c88dab5c1b1c5d280cdb3b1c93d4314f
Christopher Chang <chiggiechang@gmail.com>
2021-05-27 02:19:27 -0700

    Reformat SPELL_NAMES constant array to cover multiple lines

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -3,5 +3,11 @@ class Battle < ApplicationRecord
   has_many :battle_states
 
   # This is kind of ugly, but it works for now.
-  SPELL_NAMES = ["", "Amnesia", "Anti-spell", "Blindness", "Blindness", "Cause Heavy Wounds", "Cause Light Wounds", "Charm Monster", "Charm Person", "Clap of Lightning", "Confusion/Maladroitness", "Counter Spell", "Cure Heavy Wounds", "Cure Light Wounds", "Delay Effect", "Disease", "Dispel Magic", "Fear (No CFDS)", "Finger of Death", "Fire Storm", "Fireball", "Haste", "Ice Storm", "Invisibility", "Lightning Bolt", "Magic Mirror", "Magic Missile", "Paralysis", "Permanency", "Poison", "Protection", "Remove Enchantment", "Resist Cold", "Resist Heat", "Shield", "Summon Fire Elemental", "Summon Giant", "Summon Goblin", "Summon Ice Elemental", "Summon Ogre", "Summon Troll", "Surrender", "Time Stop"].freeze
+  SPELL_NAMES = ['', 'Amnesia', 'Anti-spell', 'Blindness', 'Blindness', 'Cause Heavy Wounds', 'Cause Light Wounds',
+                 'Charm Monster', 'Charm Person', 'Clap of Lightning', 'Confusion/Maladroitness', 'Counter Spell',
+                 'Cure Heavy Wounds', 'Cure Light Wounds', 'Delay Effect', 'Disease', 'Dispel Magic', 'Fear (No CFDS)',
+                 'Finger of Death', 'Fire Storm', 'Fireball', 'Haste', 'Ice Storm', 'Invisibility', 'Lightning Bolt',
+                 'Magic Mirror', 'Magic Missile', 'Paralysis', 'Permanency', 'Poison', 'Protection', 'Remove Enchantment',
+                 'Resist Cold', 'Resist Heat', 'Shield', 'Summon Fire Elemental', 'Summon Giant', 'Summon Goblin',
+                 'Summon Ice Elemental', 'Summon Ogre', 'Summon Troll', 'Surrender', 'Time Stop'].freeze
 end
