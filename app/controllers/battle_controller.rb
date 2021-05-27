fb5f4111ce47edf30f24af3eb8440a9d1728dae7
Christopher Chang <chiggiechang@gmail.com>
2021-05-27 01:37:07 -0700

    Set up basic left and right hand input forms

--- app/controllers/battle_controller.rb
+++ app/controllers/battle_controller.rb
@@ -1,4 +1,6 @@
 class BattleController < ApplicationController
+  before_action :correct_user, only: [:show]
+
   def search
     @available_users = User.all.select do |user|
       user != current_user
@@ -22,5 +24,14 @@ class BattleController < ApplicationController
   # GET
   def show
     @battle = Battle.find(params[:battle_id])
+
+    @targets = @battle.users.reject { |user| user == current_user }.map { |user| user.email }
+    @targets << "Nobody"
+    @targets.prepend("")
+  end
+
+  def correct_user
+    battle = current_user.battles.find_by(id: params[:battle_id])
+    redirect_to battles_path, notice: "Not authorized to access this battle." if battle.nil?
   end
 end
