fdcbb12cb790ed7d1fef737cd03d2cf0432beeb1
Christopher Chang <chiggiechang@gmail.com>
2021-05-28 02:43:12 -0700

    Use rspec to create basic surrender + stab tests

---
+++ spec/spellbinder_rules_spec.rb
@@ -0,0 +1,57 @@
+require './lib/spellbinder_rules'
+
+include SpellbinderRules
+
+describe SpellbinderRules do
+  describe '.calc_next_turn: ' do
+    describe 'Surrendering' do
+      initial_battle_states = [BattleState.new(orders_left_gesture: 'P',
+                                               orders_right_gesture: 'P', player_name: 'first@example.com'),
+                               BattleState.new(orders_left_gesture: 'P',
+                                               orders_right_gesture: 'P', player_name: 'second@example.com')]
+
+      expected_battle_states = [BattleState.new(left_hand: 'P', right_hand: 'P', health: -1, player_name: 'first@example.com'),
+                                BattleState.new(left_hand: 'P', right_hand: 'P', health: -1,
+                                                player_name: 'second@example.com')]
+
+      expected_log = [ColoredText.new('red', 'first@example.com surrenders.'),
+                      ColoredText.new('red', 'second@example.com surrenders.')]
+
+      result = SpellbinderRules.calc_next_turn(initial_battle_states)
+
+      it 'causes players to lose all health' do
+        expect(result[:log]).to eq(expected_log)
+        expect(result[:next_states]).to eq(expected_battle_states)
+      end
+    end
+
+    describe 'Stabbing players' do
+      it 'logs and damages them' do
+        initial_battle_states = [BattleState.new(orders_left_gesture: '>',
+                                                 orders_right_gesture: 'P', player_name: 'first@example.com'),
+                                 BattleState.new(orders_left_gesture: 'S',
+                                                 orders_right_gesture: 'W', player_name: 'second@example.com')]
+
+        expected_battle_states = [BattleState.new(left_hand: '>', right_hand: 'P', health: 15, player_name: 'first@example.com'),
+                                  BattleState.new(left_hand: 'S', right_hand: 'W', health: 14,
+                                                  player_name: 'second@example.com')]
+
+        expected_log = [ColoredText.new('red', 'first@example.com stabs second@example.com, dealing 1 damage.')]
+
+        result = SpellbinderRules.calc_next_turn(initial_battle_states)
+
+        expect(result[:log]).to eq(expected_log)
+        expect(result[:next_states]).to eq(expected_battle_states)
+      end
+    end
+  end
+
+  describe ColoredText do
+    it 'equals another ColoredText when its sub-values are equal' do
+      colored_text1 = ColoredText.new('red', 'first@example.com surrenders.')
+      colored_text2 = ColoredText.new('red', 'first@example.com surrenders.')
+
+      expect(colored_text1).to eq(colored_text2)
+    end
+  end
+end
